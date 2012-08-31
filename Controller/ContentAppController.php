03e2ff5f88c59844a56719466cbbec3e9cec3b5d
Jason Snider <jsnider77@gmail.com>
2012-08-31 08:53:45 -0500

    Began migrating content from core functionality into a plugin

---
+++ Controller/ContentAppController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 42Viral's parent controller layer
+ *
+ * 42Viral(tm) : The 42Viral Project (http://42viral.org)
+ * Copyright 2009-2012, 42 North Group Inc. (http://42northgroup.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2009-2012, 42 North Group Inc. (http://42northgroup.com)
+ * @link          http://42viral.org 42Viral(tm)
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @package 42viral
+ */
+
+App::uses('Controller', 'Controller');
+App::uses('File', 'Utility');
+App::uses('Scrubable', 'Lib');
+App::uses('ProfileUtil', 'Lib');
+/**
+ * 42Viral's parent controller layer
+ * @author Jason D Snider <jason.snider@42viral.org>
+ * @author Zubin Khavarian (https://github.com/zubinkhavarian)
+ * @package 42viral
+ */
+class ContentAppController extends Controller{}
